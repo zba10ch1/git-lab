6c3d1d965ff51a3f49853555b21994fa3ead8692
Carl Hartzell <zba10ch1@varg-12.student.lth.se>
2017-12-08 10:57:47 +0100

    Change author again

--- src1/project/src/MyNode.java
+++ src1/project/src/MyNode.java
@@ -3,7 +3,7 @@ package tree;
 /**
  * This is a node in a binary search tree. The node holds an integer value.
  *
- * @author Lars Torbjörn
+ * @author Lars bendix
  * @see MyTree
  */
 public class MyNode {
